cbd6e866e27cb4c62541d55680db12100873b37e
visv-in <20577139+visv-in@users.noreply.github.com>
2025-05-31 10:52:51 +0000

    UI changes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     new Chart(ctx, {
         type: 'line',
         data: {
-            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
+            labels: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026],
             datasets: [{
                 label: 'HTML5',
                 data: [2, 3, 4, 4.5, 4.5, 4.5, 5, 5, 5],
@@ -24,19 +24,25 @@
                 tension: 0.1
               },{
                 label: 'Javascript',
-                data: [0.5, 1, 2, 3, 4, 4, 4.5, 4.5, 4],
+                data: [0.5, 1, 2, 3, 3, 3, 3.5, 4.5, 4],
                 fill: false,
                 borderColor: 'rgb(252, 238, 116)',
                 tension: 0.1
               },{
                 label: 'Drupal 8/9 (Aquia Cohesion)',
-                data: [0, 0, 0, 3, 4, 4, 0, 0, 0],
+                data: [null, null, null, 3, 4, 4, null, null, null],
                 fill: false,
                 borderColor: 'rgb(167, 192, 75)',
                 tension: 0.1
               },{
+                label: 'React',
+                data: [null, null, null, null, null, null, null, null, 3],
+                fill: false,
+                borderColor: 'rgb(67, 92, 175)',
+                tension: 0.1
+              },{
                 label: 'Angular (2 to 18+)',
-                data: [3, 3.5, 2, 0, 0, 0, 0, 2.5, 3],
+                data: [3, 3.5, 2, 0, 0, 0, 0, 2.5, null],
                 fill: false,
                 borderColor: 'rgb(192, 75, 85)',
                 tension: 0.1
